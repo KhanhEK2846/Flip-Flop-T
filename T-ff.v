9191d5b84e8fcde918bd8bc383f5f8814add79c7
KhanhEK <107037350+KhanhEK2846@users.noreply.github.com>
2022-10-10 08:46:02 +0700

    Update T-ff.v

--- T-ff.v
+++ T-ff.v
@@ -1,31 +1,13 @@
-//D-latch
-module D_latch(D,Clk,Qa,Qb);
-input D,Clk;
-output Qa,Qb;
-wire S,R,S_g,R_g;
-	assign	R = ~D,
-				S = D,
-				S_g = ~(S & Clk),
-				R_g = ~(R & Clk),
-				Qa = ~(S_g & Qb),
-				Qb = ~(R_g & Qa);
-endmodule
-
-//D-ff
-module D_ff(D,Clk,Qa,Qb);
-input D,Clk;
-output Qa,Qb;
-wire Cm,Qm;
-	assign	Cm = ~Clk;
-D_latch D1(D,Cm,Qm);
-D_latch D0(Qm,Clk,Qa,Qb);
-endmodule
-
-//T-ff
-module T_ff(T,Clk,Q,Qn);
-input T,Clk;
-output Q,Qn;
-wire D;
-	assign	D = Q ^ T;
-D_ff(D,Clk,Q,Qn);
+module T_FF(Q,En,Clk,Clr);
+input En,Clk,Clr;
+output Q;
+reg Q;
+always @(posedge Clk or negedge Clr)
+if(~Clr)
+	Q <= 1'b0;
+else
+	begin
+		if(En)
+		Q <= !Q;
+	end
 endmodule
